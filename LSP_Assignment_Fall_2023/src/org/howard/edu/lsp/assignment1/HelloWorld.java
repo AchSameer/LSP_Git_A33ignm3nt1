3475ab74f373edc2fd26fbead1983b01b41c8bf7
Sameer <sameeracharya164@gmail.com>
2023-08-24 19:57:49 -0400

    Initial Commit.

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	/**
+	 * Sameer Acharya
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
